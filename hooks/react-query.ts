12d2d26591c0544e3336c03c075318bc5bfd099f
quichesama <kmbehari@gmail.com>
2024-06-07 15:52:12 -0700

    add: readme notes

--- hooks/react-query.ts
+++ hooks/react-query.ts
@@ -1,8 +1,9 @@
 import { User } from '@/types/user';
 import { useQuery } from '@tanstack/react-query';
 
+//TODO: wireup everything up.
 const useUserData = (userId?: User['id']) => {
-        
+
     if(userId == null) {
 
     }
